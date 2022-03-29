4c48c0ac4ee668db61b82c8ac77ed4a59e3a04ac
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2022-03-29 13:12:11 +0600

    performance diff

---
+++ big-o/nemo.js
@@ -0,0 +1,20 @@
+const performance = require('perf_hooks').performance;
+
+const nemo = ['nemo'];
+
+const fishColony = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank'];
+
+const large = new Array(100).fill('nemo');
+
+function findNemo(array) {
+  let t0 = performance.now();
+  for (let i = 0; i < array.length; i++) {
+    if(array[i] === 'nemo') {
+      console.log('Found Nemo');
+    }
+  }
+  let t1 = performance.now();
+  console.log('Performance = ' + (t1 - t0) + ' milliseconds');
+};
+
+findNemo(large);
