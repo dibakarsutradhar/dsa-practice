e7decb1dbe5a7832abb3987b8d549eed187345cb
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2022-04-07 00:01:32 +0600

    insert, traverseToIndex  & printList method

--- data-structures/linked-lists/myLinkedList.js
+++ data-structures/linked-lists/myLinkedList.js
@@ -1,4 +1,4 @@
-// 1 --> 10 --> 5 --> 15
+// 1 --> 10 --> 99 --> 5 --> 16 --> ... ---> 99
 
 // let myLinkedList = {
 //   head: {
@@ -44,10 +44,47 @@ class LinkedList {
     this.head = newNode;
     this.length++;
   }
+
+  traverseToIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+    
+    while(counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+
+  insert(index, value) {
+    if (index >= this.length) {
+      return this.append(value);
+    }
+
+    const newNode = new Node(value);
+    const leader = this.traverseToIndex(index - 1);
+    const pointer = leader.next;
+    leader.next = newNode;
+    newNode.next = pointer;
+    this.length++;
+    // return this.printList();
+  }
+
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    };
+    return console.log(array);
+  }
 }
 
 const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
 myLinkedList.prepend(1);
-console.log(myLinkedList);
+myLinkedList.insert(2, 99);
+myLinkedList.insert(20, 88);
+myLinkedList.printList();
