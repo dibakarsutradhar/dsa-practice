220dcaa2a4ed0346af9fd6a9eff6c0f7c0044aa7
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2022-04-15 13:53:36 +0600

    addVertex and addEdge method

--- data-structures/graphs/adjacentList.js
+++ data-structures/graphs/adjacentList.js
@@ -5,11 +5,14 @@ class Graph {
   }
 
   addVertex(node) {
-    // 
+    this.adjacentList[node] = [];
+    this.numberOfNodes++;
   }
 
   addEdge(node1, node2) {
     // undirected graph
+    this.adjacentList[node1].push(node2);
+    this.adjacentList[node2].push(node1);
   }
 
   showConnections() {
@@ -35,4 +38,23 @@ myGraph.addVertex('3');
 myGraph.addVertex('4');
 myGraph.addVertex('5');
 myGraph.addVertex('6');
+
+myGraph.addEdge('3', '1');
 myGraph.addEdge('3', '4');
+myGraph.addEdge('4', '2');
+myGraph.addEdge('4', '5');
+myGraph.addEdge('1', '2');
+myGraph.addEdge('1', '0');
+myGraph.addEdge('0', '2');
+myGraph.addEdge('6', '5');
+
+myGraph.showConnections();
+
+// Answer
+// 0--->1 2 
+// 1--->3 2 0 
+// 2--->4 1 0 
+// 3--->1 4 
+// 4--->3 2 5 
+// 5--->4 6 
+// 6--->5 
