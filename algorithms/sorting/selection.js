b5c928cff569365fe9025f50e851933eb62b6c4f
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2022-04-18 23:10:19 +0600

    selection sorting algorithm

---
+++ algorithms/sorting/selection.js
@@ -0,0 +1,22 @@
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+const selectionSort = (array) => {
+  const length = array.length;
+  for (let i = 0; i < length; i++) {
+    // set current index as minimum
+    let min = i;
+    let temp = array[i];
+    for (let j = i+1; j < length; j++) {
+      if (array[j] < array[min]) {
+        // update min if current is lower than what we had previously
+        min = j;
+      }
+    }
+    array[i] = array[min];
+    array[min] = temp;
+  }
+  return array;
+}
+
+selectionSort(numbers);
+console.log(numbers);
