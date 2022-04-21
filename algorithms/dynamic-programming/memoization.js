e6e2c2297fc1cc1091c77293fe88b22d10b6cfa8
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2022-04-21 23:15:55 +0600

    optimized with closure

--- algorithms/dynamic-programming/memoization.js
+++ algorithms/dynamic-programming/memoization.js
@@ -1,15 +1,18 @@
-let cache = {};
-
-function memoizedAddTo80(n) {
-  if (n in cache) {
-    return cache[n];
-  } else {
-    console.log('Long Time');
-    cache[n] = n + 80;
-    return cache[n];
+function memoizedAddTo80() {
+  let cache = {};
+  return function(n) {
+    if (n in cache) {
+      return cache[n];
+    } else {
+      console.log('Long Time');
+      cache[n] = n + 80;
+      return cache[n];
+    }
   }
 }
 
-console.log('1', memoizedAddTo80(5));
-console.log('2', memoizedAddTo80(6));
-console.log('3', memoizedAddTo80(6));
+const memoized = memoizedAddTo80();
+
+console.log('1', memoized(5));
+console.log('2', memoized(6));
+console.log('3', memoized(6));
